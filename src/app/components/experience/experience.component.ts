fa069115b4bf647bf0681de5358ba22a17ae00eb
Carson Rottinghaus <cqrottinghaus@gmail.com>
2022-01-08 03:23:28 -0600

    Added logos and GSAP animations to experience component

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -12,15 +12,104 @@ gsap.registerPlugin(ScrollTrigger, CSSPlugin);
 export class ExperienceComponent implements OnInit {
 
   @ViewChild('section', { static: true }) section: ElementRef<HTMLDivElement>;
+  @ViewChild('c1', { static: true }) c1: ElementRef<HTMLDivElement>;
+  @ViewChild('c2', { static: true }) c2: ElementRef<HTMLDivElement>;
+  @ViewChild('c3', { static: true }) c3: ElementRef<HTMLDivElement>;
+  @ViewChild('c4', { static: true }) c4: ElementRef<HTMLDivElement>;
+  @ViewChild('t1', { static: true }) t1: ElementRef<HTMLDivElement>;
+  @ViewChild('t2', { static: true }) t2: ElementRef<HTMLDivElement>;
+  @ViewChild('t3', { static: true }) t3: ElementRef<HTMLDivElement>;
+  @ViewChild('t4', { static: true }) t4: ElementRef<HTMLDivElement>;
+  @ViewChild('header', { static: true }) header: ElementRef<HTMLDivElement>;
+  @ViewChild('footer', { static: true }) footer: ElementRef<HTMLDivElement>;
 
   constructor() { }
 
   ngOnInit(): void {
     this.initAnimations();
+    this.initParallaxScroll();
   }
 
-  initAnimations(){
+  initAnimations(): void{
+    let timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: this.section.nativeElement,
+        toggleActions: "restart restart restart restart"
+      }
+    });
+    timeline.from(this.header.nativeElement, {
+      opacity: 0,
+      y: -20,
+      duration: 2
+    }, 0).from(this.t1.nativeElement, {
+      opacity: 0,
+      x: -30,
+      duration: 2
+    }, 0).from(this.c1.nativeElement, {
+      width: 0,
+      duration: 1,
+    }, 0).from(this.c1.nativeElement.childNodes, {
+      delay: 0.9,
+      opacity: 0,
+      x: -10,
+      stagger: 0.1
+    }, 0);
+    
+    
+    
+    
+    timeline.from(this.t2.nativeElement, {
+      opacity: 0,
+      x: -30,
+      duration: 2
+    }, 1).from(this.c2.nativeElement, {
+      width: 0,
+      duration: 1,
+    }, 1).from(this.c2.nativeElement.childNodes, {
+      delay: 0.9,
+      opacity: 0,
+      x: -10,
+      stagger: 0.1
+    }, 1);
+    
 
+    timeline.from(this.t3.nativeElement, {
+      opacity: 0,
+      x: -30,
+      duration: 2
+    }, 2).from(this.c3.nativeElement, {
+      width: 0,
+      duration: 1,
+    }, 2).from(this.c3.nativeElement.childNodes, {
+      delay: 0.9,
+      opacity: 0,
+      x: -10,
+      stagger: 0.1
+    }, 2)
+    
+    timeline.from(this.t4.nativeElement, {
+      opacity: 0,
+      x: -30,
+      duration: 2
+    }, 3).from(this.c4.nativeElement, {
+      width: 0,
+      duration: 1,
+    }, 3).from(this.c4.nativeElement.childNodes, {
+      delay: 0.9,
+      opacity: 0,
+      x: -10,
+      stagger: 0.1
+    }, 3);
+
+    timeline.from(this.footer.nativeElement.childNodes, {
+      opacity: 0,
+      duration: 1,
+    })
+
+  }
+
+
+  initParallaxScroll(): void{
     gsap.to(this.section.nativeElement, {
       backgroundPosition: '50% 25%', 
       ease: "none",
